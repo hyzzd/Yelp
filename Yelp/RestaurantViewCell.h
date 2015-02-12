67b94686b7cf2c67164fff11e49d81153104d95a
Neal Wu <nwu@box.com>
2015-02-11 23:32:02 -0800

    Update to the Pods

---
+++ Yelp/RestaurantViewCell.h
@@ -0,0 +1,13 @@
+//
+//  RestaurantViewCell.h
+//  Yelp
+//
+//  Created by Neal Wu on 2/11/15.
+//  Copyright (c) 2015 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RestaurantViewCell : UITableViewCell
+
+@end
---
+++ Yelp/RestaurantViewCell.m
@@ -0,0 +1,23 @@
+//
+//  RestaurantViewCell.m
+//  Yelp
+//
+//  Created by Neal Wu on 2/11/15.
+//  Copyright (c) 2015 codepath. All rights reserved.
+//
+
+#import "RestaurantViewCell.h"
+
+@implementation RestaurantViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
